3ef1c36ddd33fee6904c0639e96b50e56c230ba8
awais922609 <awaissajid92260@gmail.com>
2023-04-27 13:46:09 +0500

    1st Commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.6.0 < 0.9.0;
+
+contract SimpleStorage{
+    uint256 favouriteNumber = 5 ;
+
+    function store (uint256 _favouriteNumber) public   {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    
+    struct People{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    People[] public  people ;
+    People public Person = People({favouriteNumber: 1 , name: "Patrick"});
+
+    function addPerson(string memory _name , uint256 _favouriteNumber) public {
+        people.push(People({favouriteNumber: _favouriteNumber , name: _name}));
+    }
+    
+    function retrieve() public  view returns (uint256){
+        return favouriteNumber;
+    }
+
+}
