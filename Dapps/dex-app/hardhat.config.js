ef9bf9d132418f64bfda56228baabe4bf0c3f50e
awais922609 <awaissajid92260@gmail.com>
2023-08-22 11:15:31 +0500

    Commit

---
+++ Dapps/dex-app/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+
+  etherscan: {
+    apiKey:  process.env.ETHERSCAN_API_KEY,
+  },
+};
